cbd2c84f36cadd9deba693d4fa29ac547b77e38c
Chad5599 <31304760+Chad5599@users.noreply.github.com>
2017-08-25 13:16:50 +0500

    Adding check_git_1 a c sharp file I have just made

---
+++ Assets/scripts/check_git_1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check_git_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+		print ("[1] I am just checking git");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
