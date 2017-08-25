49d0fe639571f7d3b29f5d71e3f85ba2d4332f1f
Chad5599 <31304760+Chad5599@users.noreply.github.com>
2017-08-25 12:53:36 +0500

    Adding whole project 2d Car to git. Ignoring unity unnecessary Files

---
+++ Assets/scripts/enemyGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyGenerator : MonoBehaviour {
+
+	public GameObject enemyGeneratorObj;
+
+	public GameObject[] enemyCars;
+
+
+
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		
+		if(col.gameObject.tag == "enemy")
+		{
+
+			Vector3 carPos = new Vector3(Random.Range(-1.2f, 1.8f), enemyGeneratorObj.transform.position.y, enemyGeneratorObj.transform.position.z);
+			int carNumber = Random.Range(0,5);
+			GameObject enemyCarObjInstance =  Instantiate(enemyCars[carNumber], carPos, enemyGeneratorObj.transform.rotation) as GameObject;
+
+
+			enemyCarObjInstance.transform.Rotate (0,0,180);
+		}
+	}
+
+}
---
+++ Assets/scripts/moveMainPlayher.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveMainPlayher : MonoBehaviour {
+
+	public AudioClip destructiveSound;
+
+	public GameObject audiObject;
+
+	private AudioSource audioSourceMain;
+
+	public GameObject explosion;
+
+	public float speed;
+	private Vector3 positionCar;
+
+	// Use this for initialization
+	void Start () {
+		audioSourceMain = audiObject.GetComponent<AudioSource>();
+	}
+
+
+
+	IEnumerator delayForExplosion()
+	{
+		
+		Instantiate (explosion, transform.position, transform.rotation);
+
+
+		yield return new WaitForSeconds(1);
+
+
+		Destroy (this.gameObject);
+		Application.LoadLevel ("GUI_Scene");
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		
+
+		if(col.gameObject.tag == "enemy")
+		{
+
+			audioSourceMain.PlayOneShot (destructiveSound,1f);
+
+
+
+
+			Instantiate (explosion, col.gameObject.transform.position, col.gameObject.transform.rotation);
+			Destroy (col.gameObject);
+
+
+
+			StartCoroutine(delayForExplosion());
+
+
+		}
+	}
+
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		positionCar = transform.position;
+
+		if(Input.GetKey(KeyCode.D) && (positionCar.x <= 1.80f)){
+			transform.Translate(Vector3.right * Time.deltaTime*speed);
+		}
+
+		if(Input.GetKey(KeyCode.A) && (positionCar.x >= -1.80f)){
+			transform.Translate(Vector3.left * Time.deltaTime*speed);
+		}
+	}
+}
