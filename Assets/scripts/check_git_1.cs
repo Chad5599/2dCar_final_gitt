11e7dd788bf38bda54ac53a03957551277a4173a
Chad5599 <31304760+Chad5599@users.noreply.github.com>
2017-08-25 17:37:20 +0500

    again adding some changes to myFirstBranch lets see

--- Assets/scripts/check_git_1.cs
+++ Assets/scripts/check_git_1.cs
@@ -14,14 +14,14 @@ public class check_git_1 : MonoBehaviour {
 		print ("A little change for the clone");
 
 
-<<<<<<< HEAD
+
 
 		print ("I am doing changes in new branch named myFirstBranch");
 
 		print ("I am adding 1st change in myFirstBranch");
-=======
+
 		print ("[1] I am proceeding with the master ");
->>>>>>> master
+
 	}
 	
 	// Update is called once per frame
