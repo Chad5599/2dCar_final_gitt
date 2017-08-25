d60ec1180d2419baecb471e445e28f61601344f9
Chad5599 <31304760+Chad5599@users.noreply.github.com>
2017-08-25 18:30:50 +0500

    I am making myFirstBranch same

--- Assets/scripts/check_git_1.cs
+++ Assets/scripts/check_git_1.cs
@@ -1,23 +1,3 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class check_git_1 : MonoBehaviour {
-
-	void Start () {
-
-
-		print ("I am start myFirstBranch");
-
-
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		print ("I am update myFirstBranch");
-	}
-
-
-
-}
